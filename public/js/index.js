05532bae406efb2cfd07c46592d11fce6d5e8ae0
Michelle Bu <analogmidnight@gmail.com>
2012-12-19 23:22:23 -0800

    done

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
   var data_count = 0;
   $('#add').click(function() {
     var key_field = $('<input type="text" id="key-' + data_count + '">');
+    var value_label = $('<label for="value-' + data_count + '">Lists, Strings, or Numbers</label>');
     var value_field = $('<input type="text" id="value-' + data_count + '">');
+    var value_div = $('<div></div>').addClass('value').append(value_label).append(value_field);
     $('#data').append(key_field);
-    $('#data').append(value_field);
+    $('#data').append('<div class="colon">:</div>');
+    $('#data').append(value_div);
     $('#data').append($('<br/>'));
     data_count += 1;
   });

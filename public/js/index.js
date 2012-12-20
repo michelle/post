fbd7a72a64875e95cd64640fd526acce1ea919df
Michelle Bu <analogmidnight@gmail.com>
2012-12-20 10:51:24 -0800

    added functionality for different data types

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var data_count = 0;
   $('#add').click(function() {
     var key_field = $('<input type="text" id="key-' + data_count + '">');
-    var value_label = $('<label for="value-' + data_count + '">Strings, without the quotes.</label>');
+    var value_label = $('<label id="label-' + data_count + '" class="clicky" index="0">TYPE: STRING (Click to toggle)</label>');
     var value_field = $('<input type="text" id="value-' + data_count + '">');
     var value_div = $('<div></div>').addClass('value').append(value_label).append(value_field);
     $('#data').append(key_field);
@@ -11,12 +11,37 @@ $(document).ready(function() {
     $('#data').append($('<br/>'));
     data_count += 1;
   });
+
+  var types = ['Type: STRING', 'Type: ARRAY', 'Type: NUMBER'];
+  $(document).on('click', '.clicky', function() {
+    var index = (parseInt($(this).attr('index')) + 1) % 3;
+    $(this).text(types[index]);
+    $(this).attr('index', index.toString());
+    if (index == 1) {
+      $
+    }
+  });
   $('#submit').click(function() {
     var payload = {}
     for(var i = 0; i < data_count; i++) {
       var key = $('#key-' + i).val();
       if (key != '') {
-        payload[key] = $('#value-' + i).val();
+        var type = $('#label-' + i).text().split(' ').pop();
+        var value = $('#value-' + i).val();
+        if (type == 'ARRAY') {
+          value = strip_ends(value, '[');
+          value = value.split(',');
+          for (var j = 0; j < value.length; j += 1) {
+            value[i].replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+            value[i] = strip_ends(value[i], '"');
+            value[i] = strip_ends(value[i], "'");
+          }
+        } else if (type == 'NUMBER') {
+          value = parseInt(value);
+        } else {
+          value = strip_ends($('#value-' + i).val(), '"');
+        }
+        payload[key] = value;
       }
     }
     $.post('/post', {
@@ -29,4 +54,18 @@ $(document).ready(function() {
     });
     console.log(payload);
   });
+
+  function strip_ends(str, ends) {
+    var special = { '[' : ']',
+                    '(' : ')',
+                    '<' : '>',
+                    '{' : '}' };
+
+    if (str.charAt(0) == ends &&
+        (str.charAt(str.length - 1) == ends ||
+        str.charAt(str.length - 1) == special[ends])) {
+      str = str.slice(1, str.length - 1);
+    }
+    return str;
+  }
 });

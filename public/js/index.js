185b449efd0753b7f871526f8391c3e742e36697
Dan Wang <d.wang1991@gmail.com>
2012-12-19 21:43:27 -0800

    It console.logs the array

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
   var data_count = 0;
   $('#add').click(function() {
-    var key_field = $('<input type="text" id="data-' + data_count + '">');
-    var value_field = $('<input type="text" id="data-' + data_count + '">');
+    var key_field = $('<input type="text" id="key-' + data_count + '">');
+    var value_field = $('<input type="text" id="value-' + data_count + '">');
     $('#data').append(key_field);
     $('#data').append(value_field);
+    $('#data').append($('<br/>'));
     data_count += 1;
+  });
+  $('#submit').click(function() {
+    var payload = {}
+    for(var i = 0; i < data_count; i++) {
+      payload[$('#key-' + i).val()] = $('#value-' + i).val();
+    }
+    console.log(payload);
   });
 });
